c36449b4cdf4865a8524dd5f8354c4cdaf79f425
Kalk <simon.d.mansson@gmail.com>
2017-06-22 14:43:28 +0200

    Added connect and close connection functions

---
+++ src/main/kotlin/DBConnection.kt
@@ -0,0 +1,23 @@
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
+/**
+ * Created by kalk on 6/22/17.
+ */
+fun openConnection() : Connection {
+    try {
+      val connection = DriverManager.getConnection("jdbc:postgresql://localhost/4532/bradgarden?user=postgres&password=postgres")
+        return connection
+    } catch (e: SQLException) {
+        throw APIException("database connection error")
+    }
+}
+
+fun closeConnection(connection: Connection) {
+    try {
+        connection.close()
+    } catch (e: SQLException) {
+        e.printStackTrace()
+    }
+
+}
