4391c6c9a3ebd56e40a6da3cc152a2f2e114dcb6
Kalk <simon.d.mansson@gmail.com>
2017-06-26 18:19:27 +0200

    Implemented get sessions

--- src/main/kotlin/SessionDAO.kt
+++ src/main/kotlin/SessionDAO.kt
@@ -1,3 +1,5 @@
+import java.sql.PreparedStatement
+import java.sql.ResultSet
 
 /**
  * Created by kalk on 6/20/17.
@@ -5,19 +7,79 @@
 class SessionDAO  {
 
     fun add(gameID: Int, date: String, winners: Array<Int>, losers: Array<Int>, traitors: Array<Int>): Int {
-        return 0
+       val sessionID: Int
+        try {
+            val con = openConnection()
+            val insertSession  = "insert into game_session (game, session_date) values ?, ? returning session_id"
+            val insertWinner = "insert into winner values ?, ?"
+            val insertLoser = "insert into loser values ?, ?"
+            val insertTraitor = "insert into traitor values ?, ?"
+            var stmt = con.prepareStatement(insertSession)
+            stmt.setInt(1, gameID)
+            stmt.setString(2, date)
+            stmt.executeQuery()
+            stmt.resultSet.next()
+            sessionID = stmt.resultSet.getInt(1)
 
+            for (winner in winners) {
+                stmt = con.prepareStatement(insertWinner)
+                stmt.setInt(1, sessionID)
+                stmt.setInt(2, winner)
+                stmt.executeQuery()
+            }
+            for (loser in losers) {
+                stmt = con.prepareStatement(insertLoser)
+                stmt.setInt(1, sessionID)
+                stmt.setInt(2, loser)
+                stmt.executeQuery()
+            }
+            if(traitors.size > 0) {
+                for (traitor in traitors) {
+                    stmt = con.prepareStatement(insertTraitor)
+                    stmt.setInt(1, sessionID)
+                    stmt.setInt(2, traitor)
+                    stmt.executeQuery()
+                }
+            }
+        } catch (e: Exception) {
+            throw APIException("${e.message}")
+        }
+        return sessionID
     }
 
-    fun get(from:Int = 0, numOf:Int = 0): Array<Session> {
-        val sessions = arrayOf(Session(id=0,date="asd",gameID = 1,winners = arrayOf(1), losers = arrayOf(2), traitors = arrayOf(2)))
+    fun get(limit:Int = 100, offset:Int = 0): ArrayList<lightSession> {
+        val sessions = ArrayList<lightSession>()
+        try {
+            val con = openConnection()
+            val stmt = con.prepareStatement("select * from game_session limit ? offset ?")
+            stmt.setInt(1, limit)
+            stmt.setInt(2, offset)
+            val rs: ResultSet
+            rs = stmt.executeQuery()
+            while (rs.next()) {
+                sessions.add(lightSession(id = rs.getInt(1), gameID = rs.getInt(2), date = rs.getString(3)))
+            }
+            stmt.close()
+        } catch (e: Exception) {
+            throw APIException("${e.message}")
+        }
         return sessions
     }
 
     fun remove(id: Int): Boolean {
+        try {
+            val con = openConnection()
+            val stmt = con.prepareStatement("delete from game_session where session_id = ?")
+            stmt.setInt(1, id)
+            stmt.execute()
+            stmt.close()
+            return true
+        } catch (e: Exception) {
+            throw APIException("${e.message}")
+        }
         return false
     }
-
 }
 
-data class Session(val id: Int, val date: String, val gameID: Int, val winners: Array<Int>, val losers: Array<Int>, val traitors: Array<Int>)
+data class lightSession(val id: Int, val date: String, val gameID: Int)
+data class Session(val id: Int, val date: String, val gameID: Int, val winners: List<Int>, val losers: List<Int>, val traitors: List<Int>)
