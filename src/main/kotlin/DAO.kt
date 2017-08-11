42177a4cde9906ac3b1eeec055af21f815c4223a
Kalk <simon.d.mansson@gmail.com>
2017-08-11 20:37:34 +0200

    base class

---
+++ src/main/kotlin/DAO.kt
@@ -0,0 +1,16 @@
+/**
+ * Created by kalk on 8/4/17.
+ */
+class DAO {
+    //TODO class that other dao's inherit. specific behaviour is set through interfaces.
+    /**
+    val create
+    val update
+    val read
+    val delete
+
+    abstract fun add(args: Array<String>)
+    fun
+**/
+
+}
