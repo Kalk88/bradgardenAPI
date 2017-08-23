c71bad5a5a3ad4d890f5355f89abd8e72b1a895b
Kalk <simon.d.mansson@gmail.com>
2017-08-23 10:53:59 +0200

    Sql files

---
+++ sql/Brädgården_insert_members.sql
@@ -0,0 +1,6 @@
+insert into member (first_name, last_name) values
+('Simon', 'Månsson'),
+('Petter', 'Månsson'),
+('Axel', 'Månsson'),
+('Charles', 'Nordström'),
+('Jonathan', 'Hagberg')
---
+++ sql/bradgardenDB.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE bradgardendb
+
+CREATE TABLE IF NOT EXISTS member
+(
+  member_id    SMALLSERIAL PRIMARY KEY UNIQUE NOT NULL,
+  first_name   VARCHAR(30) NOT NULL,
+  last_name    VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS game
+(
+  game_id     SMALLSERIAL PRIMARY KEY UNIQUE NOT NULL,
+  game_name   VARCHAR(50) NOT NULL,
+  max_players INTEGER NOT NULL,
+  traitor     BOOLEAN NOT NULL,
+  co_op       BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS game_session
+(
+  session_id     SMALLSERIAL UNIQUE NOT NULL,
+  game           INTEGER REFERENCES game (game_id) NOT NULL,
+  session_date   VARCHAR(25) NOT NULL,
+  PRIMARY KEY (session_id)
+);
+
+CREATE TABLE IF NOT EXISTS traitor
+(
+  game_session INTEGER REFERENCES game_session (session_id) ON DELETE CASCADE NOT NULL,
+  member    INTEGER REFERENCES member (member_id) NOT NULL,
+  PRIMARY KEY (game_session,member)
+);
+
+CREATE TABLE IF NOT EXISTS loser
+(
+  game_session  INTEGER REFERENCES game_session (session_id) ON DELETE CASCADE NOT NULL,
+  member    INTEGER REFERENCES member (member_id) NOT NULL,
+  PRIMARY KEY (game_session,member)
+);
+CREATE TABLE IF NOT EXISTS winner
+(
+  game_session  INTEGER REFERENCES game_session (session_id) ON DELETE CASCADE NOT NULL,
+  member    INTEGER REFERENCES member (member_id) NOT NULL,
+  PRIMARY KEY (game_session,member)
+);
+
+CREATE TABLE IF NOT EXISTS api_user
+(
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    secret VARCHAR(100) NOT NULL,
+    PRIMARY KEY (email)
+);
