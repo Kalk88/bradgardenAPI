9b46beeda1132b1b190d03ec4704a7e78bcf2265
Kalk <simon.d.mansson@gmail.com>
2018-03-17 22:15:57 +0100

    fix

--- src/main/kotlin/MemberDAO.kt
+++ src/main/kotlin/MemberDAO.kt
@@ -1,5 +1,6 @@
 import kotlin.collections.ArrayList
 import org.apache.commons.dbutils.DbUtils
+import java.sql.Connection
 
 
 /**
@@ -28,7 +29,7 @@ class MemberDAO(private val db: Database): DAOInterface<Member> {
     override fun update(id: Int, member: Member): Boolean {
         val con = db.open()
         try {
-            val stmt = con.prepareStatement("update Member set first_name = ?, last_name = ? where member_id = ?")
+            val stmt = con.prepareStatement("update member set first_name = ?, last_name = ? where member_id = ?")
             stmt.setString(1, member.firstName)
             stmt.setString(2, member.lastName)
             stmt.setInt(3, id)
@@ -116,20 +117,21 @@ class MemberDAO(private val db: Database): DAOInterface<Member> {
     override fun getAll(): ArrayList<Member> {
         val members = ArrayList<Member>()
         val con = db.open()
+        val winQuery = """select count(member) from winner where member = ?"""
+        val lossQuery = """select count(member) from loser where member = ?"""
+        val traitorQuery = """select count(member) from traitor where member = ?"""
         try {
-            val stmt = con.prepareStatement("""select m.first, m.last, w.wins, l.losses, t.timesTraitor, m.id from
-                                                (select count(member) as wins from winner) as w,
-                                                (select count(member) as losses from loser) as l,
-                                                (select count(member) as timesTraitor from traitor) as t,
-                                                (select first_name as first, last_name as last , member_id as id from member) as m""")
+            val stmt = con.prepareStatement("""SELECT * from member""")
             val rs = stmt.executeQuery()
             while(rs.next()) {
-                val wins = rs.getInt(3)
-                val losses = rs.getInt(4)
+                val id = rs.getInt(1)
+                val wins = count(id, winQuery, con)
+                val losses = count(id, lossQuery, con)
                 val total = wins + losses
-                members.add(Member(id = rs.getInt(6), firstName = rs.getString(1), lastName = rs.getString(2),
+                val traitor = count(id, traitorQuery, con)
+                members.add(Member(id = id, firstName = rs.getString(2), lastName = rs.getString(3),
                         wins = wins, winRatio = wins.toDouble() / total, losses = losses,
-                        timesTraitor = rs.getInt(5), gamesPlayed = total))
+                        timesTraitor = traitor, gamesPlayed = total))
             }
         } catch (e: Exception) {
             throw APIException("${e.message}")
@@ -139,4 +141,10 @@ class MemberDAO(private val db: Database): DAOInterface<Member> {
         return members
     }
 
+    private fun count(id:Int, query:String, con:Connection): Int {
+        val stmt = con.prepareStatement(query)
+        stmt.setInt(1, id)
+        val rs = stmt.executeQuery()
+        return  rs.getInt(1)
+    }
 }
