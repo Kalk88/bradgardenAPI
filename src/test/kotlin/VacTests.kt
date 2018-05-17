0647a3253c476f31744acb0c52864392675b7c27
Kalk <simon.d.mansson@gmail.com>
2018-05-17 23:43:45 +0200

    updated asserts

--- src/test/kotlin/VacTests.kt
+++ src/test/kotlin/VacTests.kt
@@ -1,34 +1,36 @@
 import org.junit.Test
+import org.junit.Assert.assertFalse;
+import org.junit.Assert.assertTrue;
 
 class VacTests {
 
     @Test fun should_return_fasle_when_member_is_winner_and_loser() {
         val winners = listOf(1, 2)
         val losers = listOf(1,3,5)
-        assert(winnersInLosers(winners, losers))
+        assertTrue(winnersInLosers(winners, losers))
     }
 
     @Test fun should_return_true_when_members_is_not_in_both() {
         val winners = listOf(1, 2)
         val losers = listOf(3,5)
-        assert(!winnersInLosers(winners, losers))
+        assertFalse(winnersInLosers(winners, losers))
     }
 
     @Test fun should_return_fasle_when_traitor_is_not_in_winner_or_loser() {
         val winners = listOf(1, 2)
         val losers = listOf(3,5)
         val traitors = listOf(3,7)
-        assert(!traitorIsInWinnersOrLosers(traitors,winners, losers))
+        assertFalse(traitorIsInWinnersOrLosers(traitors,winners, losers))
     }
 
     @Test fun should_return_true_when_traitor_is_in_one_of_winners_or_losers() {
         val winners = listOf(1, 2)
         val losers = listOf(3,5)
         val traitors = listOf(3)
-        assert(traitorIsInWinnersOrLosers(traitors,winners, losers))
+        assertTrue(traitorIsInWinnersOrLosers(traitors,winners, losers))
 
         val traitors2 = listOf(1)
-        assert(traitorIsInWinnersOrLosers(traitors2, winners, losers))
+        assertTrue(traitorIsInWinnersOrLosers(traitors2, winners, losers))
     }
 }
 
