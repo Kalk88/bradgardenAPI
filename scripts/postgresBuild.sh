7cdc5e31985aedc8e3608ebc743fc22f0e632f51
Kalk <simon.d.mansson@gmail.com>
2018-05-17 17:47:55 +0200

    dredd and scripts

--- scripts/postgresBuild.sh
+++ scripts/postgresBuild.sh
@@ -3,5 +3,5 @@ docker pull postgres
 sudo docker run -d -p 127.0.0.2:5432:5432 --name bradgardenDB -d postgres:latest
 echo Sleeping 5s before loading data
 sleep 5s
-cat  sql/insert_games.sql sql/insert_members.sql sql/insert_sessions.sql | psql -h 127.0.0.2 -p 5432 -U postgres -f sql/bradgardenDB.sql
+cat sql/bradgardenDB.sql  sql/insert_games.sql sql/insert_members.sql sql/insert_sessions.sql | psql -h 127.0.0.2 -p 5432 -U postgres
 echo Data loaded
